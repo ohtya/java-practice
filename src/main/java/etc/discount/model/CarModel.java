f4e43c7996e578bba832c67f82260ebb87f0b3c8
ytttt-2kab <mogyo124@gmail.com>
2020-02-09 14:25:03 +0900

    車種enum追加

---
+++ src/main/java/etc/discount/model/CarModel.java
@@ -0,0 +1,27 @@
+package etc.discount.model;
+
+/**
+ * 車種
+ */
+public enum CarModel {
+    /**
+     * 軽自動車等
+     */
+    MINI,
+    /**
+     * 普通車
+     */
+    ORDINARY_CAR,
+    /**
+     * 中型車
+     */
+    MIDIUM_SIZED_CAR,
+    /**
+     * 大型車
+     */
+    LARGE_CAR,
+    /**
+     * 特大車
+     */
+    EXTRA_LARGE_CAR
+}
