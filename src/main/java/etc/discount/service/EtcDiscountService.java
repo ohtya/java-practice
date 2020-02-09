a696675c3000baf22df52f823d214cca338d1b42
ohtya <otyatya0519@gmail.com>
2020-02-09 21:03:46 +0900

    feat: DiscountService の実装を追加

---
+++ src/main/java/etc/discount/service/EtcDiscountService.java
@@ -0,0 +1,14 @@
+package etc.discount.service;
+
+public class EtcDiscountService implements DiscountService {
+
+    @Override
+    public long calculate() {
+        return 0;
+    }
+
+    @Override
+    public long add(long x, long y) {
+        return 0;
+    }
+}
