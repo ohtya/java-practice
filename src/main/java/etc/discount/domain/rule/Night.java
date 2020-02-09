b6f383ad9bb1e4252bc68cde3a969db10775d381
ohtya <otyatya0519@gmail.com>
2020-02-10 00:10:45 +0900

    feat: 深夜割引クラスを追加

---
+++ src/main/java/etc/discount/domain/rule/Night.java
@@ -0,0 +1,33 @@
+package etc.discount.domain.rule;
+
+import etc.discount.domain.DiscountRule;
+
+/**
+ * 深夜割引
+ * https://www.driveplaza.com/traffic/tolls_etc/etc_dis_night/
+ * <p>
+ * 深夜とは、毎日0時から4時までを指します
+ */
+public class Night implements DiscountRule {
+
+    /**
+     * 入り口料金所または出口料金所を深夜に通過した場合に適用可能です
+     *
+     * @return true: 可能, false: 不可
+     */
+    @Override
+    public boolean isApplicable() {
+        return false;
+    }
+
+    /**
+     * 以下のルールで割引率を適用します
+     * - 割引率30%
+     *
+     * @return 割引率
+     */
+    @Override
+    public long discountRate() {
+        return 0;
+    }
+}
