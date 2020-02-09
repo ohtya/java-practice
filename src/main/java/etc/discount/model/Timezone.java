eb619948d5ddc9f863fa2a6db8a826431b5c3192
TAKUI MASUI <kiiroitori1990.02@gmail.com>
2020-02-09 14:31:42 +0900

    コミットテスト、ENUMの追加

---
+++ src/main/java/etc/discount/model/Timezone.java
@@ -0,0 +1,20 @@
+package etc.discount.model;
+
+
+/**
+ * 時間帯
+ */
+public enum Timezone {
+    /**
+     * 朝：6～9時
+     */
+    MORNING,
+    /**
+     * 夕方：17～20時
+     */
+    EVENING,
+    /**
+     * その他
+     */
+    ETC
+}
