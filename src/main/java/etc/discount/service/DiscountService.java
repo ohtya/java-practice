28ace975acab340b8c087a838881b646fd31e367
ohtya <otyatya0519@gmail.com>
2020-02-09 13:34:00 +0900

    feat: 割引サービスのインタフェースを追加

---
+++ src/main/java/etc/discount/service/DiscountService.java
@@ -0,0 +1,14 @@
+package etc.discount.service;
+
+/**
+ * 割引を実現するサービス.
+ */
+public interface DiscountService {
+    /**
+     * 割引率を計算します
+     * FIXME: パラメータ
+     *
+     * @return 割引率
+     */
+    long calculate();
+}
