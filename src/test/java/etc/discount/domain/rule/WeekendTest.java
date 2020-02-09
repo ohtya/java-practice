d25bfab4e9aed00c1b0d435d22e183a362fd1647
ohtya <otyatya0519@gmail.com>
2020-02-10 00:10:36 +0900

    test: 休日割引テストを追加

---
+++ src/test/java/etc/discount/domain/rule/WeekendTest.java
@@ -0,0 +1,34 @@
+package etc.discount.domain.rule;
+
+import etc.discount.domain.DiscountRule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * {@link Weekend} unit test
+ */
+class WeekendTest {
+
+    private DiscountRule rule;
+
+    @BeforeEach
+    void setup() {
+        rule = new Weekend();
+    }
+
+    @Test
+    void isApplicable() {
+        final var expected = false;
+        final var actual = rule.isApplicable();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void discountRate() {
+        final var expected = 0L;
+        final var actual = rule.discountRate();
+        assertEquals(expected, actual);
+    }
+}
