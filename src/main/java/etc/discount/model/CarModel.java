e2a4432ac5713bbe76b4915801ada45882ed248a
ohtya <otyatya0519@gmail.com>
2020-02-09 14:25:13 +0900

    feat: 車種の enum を追加

---
+++ src/main/java/etc/discount/model/CarModel.java
@@ -0,0 +1,28 @@
+package etc.discount.model;
+
+/**
+ * 車種区分
+ * https://www.w-nexco.co.jp/search/carmodel/
+ */
+public enum CarModel {
+    /**
+     * 軽自動車
+     */
+    LIGHT,
+    /**
+     * 普通車
+     */
+    STANDARD,
+    /**
+     * 中型車
+     */
+    MEDIUM,
+    /**
+     * 大型車
+     */
+    LARGE,
+    /**
+     * 特大車
+     */
+    OVERSIZED
+}
