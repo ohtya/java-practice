bd3448a6302b7f9a350939234ddbe9a0a6e3651c
ohtya <otyatya0519@gmail.com>
2020-02-19 00:08:58 +0900

    feat: 休日割引を実装

--- src/main/java/etc/discount/domain/rule/Weekend.java
+++ src/main/java/etc/discount/domain/rule/Weekend.java
@@ -3,6 +3,13 @@ package etc.discount.domain.rule;
 import etc.discount.domain.DiscountRule;
 import etc.discount.model.CarModel;
 import etc.discount.model.DriveData;
+import etc.discount.model.Route;
+
+import java.time.DayOfWeek;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * 休日割引
@@ -13,8 +20,12 @@ import etc.discount.model.DriveData;
  * - 日曜日
  * - 祝日
  * - 三が日(毎年1月２日、3日)
+ * FIXME: 祝日と三が日を休日の対象にする
  */
 public class Weekend implements DiscountRule {
+
+    protected static final List<DayOfWeek> APPLICABLE_DAY_OF_WEEK = Arrays.stream(DayOfWeek.values()).filter(dayOfWeek -> dayOfWeek.equals(DayOfWeek.SATURDAY) || dayOfWeek.equals(DayOfWeek.SUNDAY)).collect(Collectors.toList());
+    protected static final List<CarModel> APPLICABLE_CAR_MODEL = Arrays.stream(CarModel.values()).filter(carModel -> carModel.equals(CarModel.KEI) || carModel.equals(CarModel.ORDINARY)).collect(Collectors.toList());
 
     /**
      * 入り口料金所または出口料金所を休日に通過した場合に適用可能です
@@ -27,7 +38,11 @@ public class Weekend implements DiscountRule {
      */
     @Override
     public boolean isApplicable(final DriveData drive) {
-        return false;
+        if (drive == null) {
+            throw new IllegalArgumentException("arg1 can not be null.");
+        }
+
+        return Objects.equals(drive.getRoute(), Route.LOCAL) && APPLICABLE_CAR_MODEL.contains(drive.getModel()) && (APPLICABLE_DAY_OF_WEEK.contains(drive.getAdmissionAt().getDayOfWeek()) || APPLICABLE_DAY_OF_WEEK.contains(drive.getExitAt().getDayOfWeek()));
     }
 
     /**
@@ -38,6 +53,6 @@ public class Weekend implements DiscountRule {
      */
     @Override
     public long discountRate() {
-        return 0;
+        return 30;
     }
 }
