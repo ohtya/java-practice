823748b220f237e152fb52a3dcd396a6e67ba414
ohtya <otyatya0519@gmail.com>
2020-02-21 23:25:23 +0900

    feat: 休日判定クラスを実装

--- src/main/java/etc/discount/domain/rule/Weekend.java
+++ src/main/java/etc/discount/domain/rule/Weekend.java
@@ -1,11 +1,11 @@
 package etc.discount.domain.rule;
 
+import etc.discount.Holiday;
 import etc.discount.domain.DiscountRule;
 import etc.discount.model.CarModel;
 import etc.discount.model.DriveData;
 import etc.discount.model.Route;
 
-import java.time.DayOfWeek;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -24,8 +24,8 @@ import java.util.stream.Collectors;
  */
 public class Weekend implements DiscountRule {
 
-    protected static final List<DayOfWeek> APPLICABLE_DAY_OF_WEEK = Arrays.stream(DayOfWeek.values()).filter(dayOfWeek -> dayOfWeek.equals(DayOfWeek.SATURDAY) || dayOfWeek.equals(DayOfWeek.SUNDAY)).collect(Collectors.toList());
     protected static final List<CarModel> APPLICABLE_CAR_MODEL = Arrays.stream(CarModel.values()).filter(carModel -> carModel.equals(CarModel.KEI) || carModel.equals(CarModel.ORDINARY)).collect(Collectors.toList());
+    private Holiday holiday;
 
     /**
      * 入り口料金所または出口料金所を休日に通過した場合に適用可能です
@@ -45,7 +45,7 @@ public class Weekend implements DiscountRule {
 
         return Objects.equals(drive.getRoute(), Route.LOCAL) &&
             APPLICABLE_CAR_MODEL.contains(drive.getModel()) &&
-            (APPLICABLE_DAY_OF_WEEK.contains(drive.getAdmissionAt().getDayOfWeek()) || APPLICABLE_DAY_OF_WEEK.contains(drive.getExitAt().getDayOfWeek()));
+            (holiday.isWeekend(drive.getAdmissionAt()) || holiday.isWeekend(drive.getExitAt()));
     }
 
     /**
