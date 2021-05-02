6c7eede84547bec5c5d4ee39ca0bd3713d75da4a
ohtya <48324058+ohtya@users.noreply.github.com>
2021-05-02 23:33:13 +0900

    feat: CinemaCitizenSenior のリファクタリング

--- src/main/java/cinema/ticket/domain/rule/CinemaCitizenSenior.java
+++ src/main/java/cinema/ticket/domain/rule/CinemaCitizenSenior.java
@@ -5,37 +5,20 @@ import cinema.ticket.model.Visitor;
 
 import java.time.LocalDateTime;
 
+import static cinema.ticket.model.VisitorType.CINEMA_CITIZEN_SENIOR;
+
 /**
  * シネマシティズン(シニア)料金算出
  */
 public class CinemaCitizenSenior implements DiscountRule {
 
-    /**
-     * 該当するかどうか
-     */
+    @Override
     public boolean isApplicable(final Visitor visitor) {
-        return (visitor.isKaiin() && visitor.isSenior());
+        return visitor.type() == CINEMA_CITIZEN_SENIOR;
     }
 
-    /**
-     * チケット料金を算出する
-     *
-     * @return チケット料金
-     * @param nowDateTime
-     */
-    public long discountRate(LocalDateTime nowDateTime, Visitor visitor) {
-        if (nowDateTime.getDayOfMonth() == 1){
-            // 映画の日
-            return 1000;
-        }
-        // TODO : 祝日対応
-        if (WEEKEND_DAY_OF_WEEK.contains(nowDateTime.getDayOfWeek())) {
-            // 土日祝の場合
-            // レイトショーであるかどうか
-            return (nowDateTime.getHour() < 20) ? 1000 : 1000;
-        } else {
-            // 平日は値段が一律
-            return (nowDateTime.getHour() < 20) ? 1000 : 1000;
-        }
+    @Override
+    public long discountRate(final LocalDateTime nowDateTime, final Visitor visitor) {
+        return 1000L;
     }
 }
