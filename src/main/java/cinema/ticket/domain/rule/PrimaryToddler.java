c164098caf63d00a47db3b94bfc1a9c166bdfff2
ohtya <48324058+ohtya@users.noreply.github.com>
2021-05-03 00:17:33 +0900

    refactor: 幼児・小学生料金のリファクタリング

--- src/main/java/cinema/ticket/domain/rule/PrimaryToddler.java
+++ src/main/java/cinema/ticket/domain/rule/PrimaryToddler.java
@@ -5,40 +5,44 @@ import cinema.ticket.model.Visitor;
 import cinema.ticket.model.VisitorType;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static cinema.ticket.model.VisitorType.PRIMARY_SCHOOL;
+import static cinema.ticket.model.VisitorType.TODDLER;
 
 /**
- * 小学生・幼児
+ * 幼児・小学生料金<br>
+ * 幼児、または、小学生の場合に適用する料金ルールです<br>
  */
 public class PrimaryToddler implements DiscountRule {
 
-    protected static final List<VisitorType> TARGET_VISITOR_TYPE = Arrays
-            .stream(VisitorType.values())
-            .filter(visitorType -> visitorType.equals(VisitorType.PRIMARY_SCHOOL) || visitorType.equals(VisitorType.TODDLER))
-            .collect(Collectors.toList());
+    protected static final List<VisitorType> TARGET_VISITOR_TYPE = List.of(PRIMARY_SCHOOL, TODDLER);
 
     @Override
     public boolean isApplicable(Visitor visitor) {
-        return TARGET_VISITOR_TYPE.contains(visitor.getVisitorType());
+        return TARGET_VISITOR_TYPE.contains(visitor.type());
     }
 
+    /**
+     * 適用可能な料金の中から最も安い料金を返します<br>
+     * <br>
+     * 平日<br>
+     * 20:00 までは 1,000 円<br>
+     * 20:00 以降は 1,000 円<br>
+     * <br>
+     * 土日祝日<br>
+     * 20:00 までは 1,000 円<br>
+     * 20:00 以降は 1,000 円<br>
+     * <br>
+     * 映画の日(毎月1日)<br>
+     * 時間帯によらず 1,000 円<br>
+     *
+     * @param nowDateTime 現在日時
+     * @param visitor     {@link Visitor}
+     * @return 料金
+     */
     @Override
     public long discountRate(LocalDateTime nowDateTime, Visitor visitor) {
-        // 幼児の値段は一律
-        // 映画の日
-        if (nowDateTime.getDayOfMonth() == 1) {
-            return 1000;
-        }
-
-        // 休日であるか
-        if (WEEKEND_DAY_OF_WEEK.contains(nowDateTime.getDayOfWeek())) {
-            // レイトショーであるかどうか
-            return (nowDateTime.getHour() < 20) ? 1000 : 1000;
-        } else {
-            // 平日の場合
-            return (nowDateTime.getHour() < 20) ? 1000 : 1000;
-        }
+        return 1000L;
     }
 }
