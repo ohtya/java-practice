71c12362cc31e38278df9d39ed7f803c130a4e20
ohtya <48324058+ohtya@users.noreply.github.com>
2021-05-03 01:04:03 +0900

    test: 乳児料金クラスのユニットテストを新規作成

---
+++ src/test/java/cinema/ticket/domain/rule/BabyTest.java
@@ -0,0 +1,132 @@
+
+package cinema.ticket.domain.rule;
+
+import cinema.ticket.domain.DiscountRule;
+import cinema.ticket.model.Visitor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * {@link Baby} unit test.
+ */
+class BabyTest {
+
+    private DiscountRule rule;
+
+    @BeforeEach
+    void setUp() {
+        rule = new Baby();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(IsApplicableArgumentsProvider.class)
+    void isApplicable(final Visitor visitor, final boolean expected) {
+        final var actual = rule.isApplicable(visitor);
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(DiscountRuleArgumentsProvider.class)
+    void isApplicable(final LocalDateTime now, final long expected) {
+        final var visitor = Visitor.builder()
+                .isKaiin(true)
+                .age(60)
+                .build();
+        final var actual = rule.discountRate(now, visitor);
+        assertEquals(expected, actual);
+    }
+
+    private static class IsApplicableArgumentsProvider implements ArgumentsProvider {
+
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+
+            return Stream.of(
+                    // 乳児である
+                    Arguments.of(
+                            Visitor.builder()
+                                    .age(0)
+                                    .build(),
+                            true),
+                    Arguments.of(
+                            Visitor.builder()
+                                    .age(2)
+                                    .build(),
+                            true),
+                    // 乳児ではない
+                    Arguments.of(
+                            Visitor.builder()
+                                    .age(3)
+                                    .build(),
+                            false)
+            );
+        }
+    }
+
+    private static class DiscountRuleArgumentsProvider implements ArgumentsProvider {
+
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    // 平日 20:00 まで
+                    Arguments.of(
+                            LocalDateTime.of(2021, 4, 26, 19, 59),
+                            0),
+                    // 平日 20:00 以降
+                    Arguments.of(
+                            LocalDateTime.of(2021, 4, 26, 20, 0),
+                            0),
+                    // 土 20:00 まで
+                    Arguments.of(
+                            LocalDateTime.of(2021, 4, 24, 19, 59),
+                            0),
+                    // 土 20:00 以降
+                    Arguments.of(
+                            LocalDateTime.of(2021, 4, 24, 20, 0),
+                            0),
+                    // 日 20:00 まで
+                    Arguments.of(
+                            LocalDateTime.of(2021, 5, 2, 19, 59),
+                            0),
+                    // 日 20:00 以降
+                    Arguments.of(
+                            LocalDateTime.of(2021, 5, 2, 20, 0),
+                            0),
+// FIXME: 祝日は未実装のため、一旦テストをコメントアウトします
+//                // 祝日 20:00 まで
+//                Arguments.of(
+//                        LocalDateTime.of(2021, 5, 3, 19, 59),
+//                        1000),
+//                // 祝日 20:00 以降
+//                Arguments.of(
+//                        LocalDateTime.of(2021, 5, 3, 20, 0),
+//                        1000),
+                    // 映画の日(毎月1日)
+                    Arguments.of(
+                            LocalDateTime.of(2021, 5, 1, 0, 0),
+                            0),
+                    Arguments.of(
+                            LocalDateTime.of(2021, 5, 1, 19, 59),
+                            0),
+                    // 映画の日(毎月1日) でもより安い料金に一致する場合はそちらを優先
+                    Arguments.of(
+                            LocalDateTime.of(2021, 5, 1, 20, 0),
+                            0)
+            );
+        }
+    }
+}
