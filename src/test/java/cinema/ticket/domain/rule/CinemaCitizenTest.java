090d881e67daa0c988ad5c6fc76ea518a096bf1a
ohtya <48324058+ohtya@users.noreply.github.com>
2021-05-02 23:59:48 +0900

    refactor: CinemaCitizenTest のリファクタリング
    - ParameterizedTest 用のクラスを inner クラスに変更
    - VisitorType を利用しないように修正

--- src/test/java/cinema/ticket/domain/rule/CinemaCitizenTest.java
+++ src/test/java/cinema/ticket/domain/rule/CinemaCitizenTest.java
@@ -2,7 +2,6 @@ package cinema.ticket.domain.rule;
 
 import cinema.ticket.domain.DiscountRule;
 import cinema.ticket.model.Visitor;
-import cinema.ticket.model.VisitorType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -43,97 +42,93 @@ class CinemaCitizenTest {
     @ArgumentsSource(CinemaCitizenDiscountRateArgumentsProvider.class)
     void discountRate(final LocalDateTime now, final long expected) {
         final var visitor = Visitor.builder()
-                .visitorType(VisitorType.GENERAL)
                 .isKaiin(true)
                 .age(59)
                 .build();
         final var actual = rule.discountRate(now, visitor);
         assertEquals(expected, actual);
     }
-}
 
-class CinemaCitizenIsApplicableArgumentsProvider implements ArgumentsProvider {
+    static class CinemaCitizenIsApplicableArgumentsProvider implements ArgumentsProvider {
 
-    @Override
-    public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
-        return Stream.of(
-                // シネマシティズン会員である
-                Arguments.of(
-                        Visitor.builder()
-                                .visitorType(VisitorType.GENERAL)
-                                .isKaiin(true)
-                                .age(59)
-                                .build(),
-                        true),
-                // シネマシティズン会員ではない
-                Arguments.of(
-                        Visitor.builder()
-                                .visitorType(VisitorType.GENERAL)
-                                .isKaiin(false)
-                                .age(59)
-                                .build(),
-                        false),
-                // シネマシティズン会員であるが、シネマシティズンシニア扱いとする
-                Arguments.of(
-                        Visitor.builder()
-                                .visitorType(VisitorType.GENERAL)
-                                .isKaiin(true)
-                                .age(60)
-                                .build(),
-                        false)
-        );
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    // シネマシティズン会員である
+                    Arguments.of(
+                            Visitor.builder()
+                                    .isKaiin(true)
+                                    .age(59)
+                                    .build(),
+                            true),
+                    // シネマシティズン会員ではない
+                    Arguments.of(
+                            Visitor.builder()
+                                    .isKaiin(false)
+                                    .age(59)
+                                    .build(),
+                            false),
+                    // シネマシティズン会員であるが、シネマシティズンシニア扱いとする
+                    Arguments.of(
+                            Visitor.builder()
+                                    .isKaiin(true)
+                                    .age(60)
+                                    .build(),
+                            false)
+            );
+        }
     }
-}
 
-class CinemaCitizenDiscountRateArgumentsProvider implements ArgumentsProvider {
+    static class CinemaCitizenDiscountRateArgumentsProvider implements ArgumentsProvider {
 
-    @Override
-    public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
-        return Stream.of(
-                // 平日 20:00 までは 1,000 円
-                Arguments.of(
-                        LocalDateTime.of(2021, 4, 26, 19, 59),
-                        1000),
-                // 平日 20:00 以降は 1,000 円
-                Arguments.of(
-                        LocalDateTime.of(2021, 4, 26, 20, 0),
-                        1000),
-                // 土 20:00 までは 1,300 円
-                Arguments.of(
-                        LocalDateTime.of(2021, 4, 24, 19, 59),
-                        1300),
-                // 土 20:00 以降は 1,000 円
-                Arguments.of(
-                        LocalDateTime.of(2021, 4, 24, 20, 0),
-                        1000),
-                // 日 20:00 までは 1,300 円
-                Arguments.of(
-                        LocalDateTime.of(2021, 5, 2, 19, 59),
-                        1300),
-                // 日 20:00 以降は 1,000 円
-                Arguments.of(
-                        LocalDateTime.of(2021, 5, 2, 20, 0),
-                        1000),
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    // 平日 20:00 まで
+                    Arguments.of(
+                            LocalDateTime.of(2021, 4, 26, 19, 59),
+                            1000),
+                    // 平日 20:00 以降
+                    Arguments.of(
+                            LocalDateTime.of(2021, 4, 26, 20, 0),
+                            1000),
+                    // 土 20:00 まで
+                    Arguments.of(
+                            LocalDateTime.of(2021, 4, 24, 19, 59),
+                            1300),
+                    // 土 20:00 以降
+                    Arguments.of(
+                            LocalDateTime.of(2021, 4, 24, 20, 0),
+                            1000),
+                    // 日 20:00 まで
+                    Arguments.of(
+                            LocalDateTime.of(2021, 5, 2, 19, 59),
+                            1300),
+                    // 日 20:00 以降
+                    Arguments.of(
+                            LocalDateTime.of(2021, 5, 2, 20, 0),
+                            1000),
 // FIXME: 祝日は未実装のため、一旦テストをコメントアウトします
-//                // 祝日 20:00 までは 1,300 円
+//                // 祝日 20:00 まで
 //                Arguments.of(
 //                        LocalDateTime.of(2021, 5, 3, 19, 59),
 //                        1300),
-//                // 祝日 20:00 以降は 1,000 円
+//                // 祝日 20:00 以降
 //                Arguments.of(
 //                        LocalDateTime.of(2021, 5, 3, 20, 0),
 //                        1000),
-                // 映画の日(毎月1日) 時間帯によらず 1,100 円
-                Arguments.of(
-                        LocalDateTime.of(2021, 5, 1, 0, 0),
-                        1100),
-                Arguments.of(
-                        LocalDateTime.of(2021, 5, 1, 19, 59),
-                        1100),
-                // 映画の日(毎月1日) でもより安い料金に一致する場合はそちらを優先
-                Arguments.of(
-                        LocalDateTime.of(2021, 5, 1, 20, 00),
-                        1000)
-        );
+                    // 映画の日(毎月1日)
+                    Arguments.of(
+                            LocalDateTime.of(2021, 5, 1, 0, 0),
+                            1100),
+                    Arguments.of(
+                            LocalDateTime.of(2021, 5, 1, 19, 59),
+                            1100),
+                    // 映画の日(毎月1日) でもより安い料金に一致する場合はそちらを優先
+                    Arguments.of(
+                            LocalDateTime.of(2021, 5, 1, 20, 0),
+                            1000)
+            );
+        }
     }
 }
