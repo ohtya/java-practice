2fec4619fe37f7d696f56b880a4f8ed5bb9f0801
ohtya <48324058+ohtya@users.noreply.github.com>
2021-05-03 00:57:14 +0900

    feat: 障がい者(学生以上)料金クラスを新規作成

--- src/main/java/cinema/ticket/domain/rule/Handicapped.java
+++ src/main/java/cinema/ticket/domain/rule/Handicapped.java
@@ -2,24 +2,20 @@ package cinema.ticket.domain.rule;
 
 import cinema.ticket.domain.DiscountRule;
 import cinema.ticket.model.Visitor;
-import cinema.ticket.model.VisitorType;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
+import static cinema.ticket.model.VisitorType.HANDICAPPED;
+
+/**
+ * 障がい者(学生以上)料金<br>
+ * 障がい者(学生以上)の場合に適用する料金ルールです<br>
+ */
 public class Handicapped implements DiscountRule {
-
-    // 中・高校生の配列
-    protected static final List<VisitorType> TARGET_VISITOR_TYPE = Arrays
-            .stream(VisitorType.values())
-            .filter(visitorType -> visitorType.equals(VisitorType.JUNIOR_HIGH_SCHOOL) || visitorType.equals(VisitorType.HIGH_SCHOOL))
-            .collect(Collectors.toList());
 
     @Override
     public boolean isApplicable(Visitor visitor) {
-        return visitor.isSyougaisya();
+        return visitor.type() == HANDICAPPED;
     }
 
     @Override
