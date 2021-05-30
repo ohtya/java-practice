3bc992b7e745aadbdc3d0b737b3e7a2ec69cd7fe
TAKUI MASUI <kiiroitori1990.02@gmail.com>
2021-05-31 00:11:47 +0900

    シニアのテスト実装。 テストがちゃんと動いていない気がする。 (+1 squashed commits)
    Squashed commits:
    
    [d65a005] シニアのテストを実装。
    テストちゃんと動いてない気もする。

---
+++ src/test/java/cinema/ticket/domain/rule/SeniorTest.java
@@ -0,0 +1,131 @@
+package cinema.ticket.domain.rule;
+
+import cinema.ticket.domain.DiscountRule;
+import cinema.ticket.model.Visitor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * {@link Senior} unit test.
+ */
+public class SeniorTest {
+
+    private DiscountRule rule;
+
+    @BeforeEach
+    void setUp() {
+        rule = new Senior();
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(IsApplicableArgumentsProvider.class)
+    void isApplicable(final Visitor visitor, final boolean expected) {
+        final var actual = rule.isApplicable(visitor);
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(DiscountRuleArgumentsProvider.class)
+    void disountRule(final LocalDateTime now, final long expected){
+        final var visitor = Visitor.builder()
+                .isKaiin(false)
+                .age(70)
+                .build();
+        final var actual = rule.discountRate(now, visitor);
+        assertEquals(expected, actual);
+
+    }
+
+    private static class IsApplicableArgumentsProvider implements ArgumentsProvider {
+
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    // シニアである
+                    Arguments.of(
+                        Visitor.builder()
+                            .age(70)
+                            .build(),
+                        true),
+                    /* シニアでない */
+                    // シネマシティズンシニアとなるケース
+                    Arguments.of(
+                        Visitor.builder()
+                            .age(70)
+                            .isKaiin(true)
+                            .build(),
+                        false
+                    ),
+                    // 単純にシニアの年齢対象ではない
+                    Arguments.of(
+                        Visitor.builder()
+                            .age(69)
+                            .isKaiin(false)
+                            .build(),
+                        false
+                    ),
+                    Arguments.of(
+                        Visitor.builder()
+                            .age(19)
+                            .isKaiin(false)
+                            .build(),
+                        false
+                    )
+            );
+        }
+    }
+
+    private static class DiscountRuleArgumentsProvider implements ArgumentsProvider {
+
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    // 平日20時まで
+                    Arguments.of(
+                            LocalDateTime.of(2021, 5, 28, 19, 59),
+                            1100
+                    ),
+                    // 平日20時以降
+                    Arguments.of(
+                            LocalDateTime.of(2021, 5, 28, 20, 00),
+                            1100
+                    ),
+                    // 土曜20時まで
+                    Arguments.of(
+                            LocalDateTime.of(2021, 5, 29, 19, 59),
+                            1100
+                    ),
+                    // 土曜20時以降
+                    Arguments.of(
+                            LocalDateTime.of(2021, 5, 29, 20, 00),
+                            1100
+                    ),
+                    // 日曜20時まで
+                    Arguments.of(
+                            LocalDateTime.of(2021, 5, 30, 19, 59),
+                            1100
+                    ),
+                    // 日曜20時以降
+                    Arguments.of(
+                            LocalDateTime.of(2021, 5, 30, 20, 00),
+                            1100
+                    )
+            );
+        }
+    }
+}
