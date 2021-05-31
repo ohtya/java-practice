8f00fc8714cc1fccd92e153a4dcf3134f42b7688
TAKUI MASUI <kiiroitori1990.02@gmail.com>
2021-05-31 22:53:13 +0900

    レビュー指摘を修正

--- src/main/java/cinema/ticket/domain/rule/General.java
+++ src/main/java/cinema/ticket/domain/rule/General.java
@@ -18,35 +18,27 @@ import java.util.stream.Collectors;
  */
 public class General implements DiscountRule {
 
-    // 一般・シニア
-    protected static final List<VisitorType> TARGET_VISITOR_TYPE = Arrays
-            .stream(VisitorType.values())
-            .filter(visitorType -> visitorType.equals(VisitorType.GENERAL))
-            .collect(Collectors.toList());
     /**
      * 適応可能であるかどうか
      *
      * @return
      */
+    @Override
     public boolean isApplicable(final Visitor visitor) {
-        return TARGET_VISITOR_TYPE.contains(visitor.getVisitorType());
+        return visitor.type() == VisitorType.GENERAL;
     }
 
     @Override
     public long discountRate(LocalDateTime nowDateTime, Visitor visitor) {
         var priceList = new ArrayList<Long>();
 
-        // 一般の値段決定
-        // 映画の日
         if (nowDateTime.getDayOfMonth() == 1) {
             priceList.add(1100L);
         }
 
-        // 休日であるか
         if (nowDateTime.getHour() < 20) {
             priceList.add(1800L);
         }
-
         return priceList.stream().min(Long::compareTo).orElse(1300L);
     }
 }
