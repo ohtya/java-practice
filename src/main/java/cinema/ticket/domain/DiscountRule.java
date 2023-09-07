8ebac865ee2b41151d8454b35283dc8972a3ab64
ohtya <otyatya0519@gmail.com>
2023-09-07 13:16:17 +0900

    feat: 不要になったため削除しました

--- src/main/java/cinema/ticket/domain/DiscountRule.java
+++ src/main/java/cinema/ticket/domain/DiscountRule.java
@@ -2,20 +2,10 @@ package cinema.ticket.domain;
 
 import cinema.ticket.model.Visitor;
 
-import java.time.DayOfWeek;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 /**
- * 割引ルール
+ * 割引ルールインタフェース
  */
 public interface DiscountRule {
-
-    List<DayOfWeek> WEEKEND_DAY_OF_WEEK = Arrays
-            .stream(DayOfWeek.values())
-            .filter(dayOfWeek -> dayOfWeek.equals(DayOfWeek.SATURDAY) || dayOfWeek.equals(DayOfWeek.SUNDAY))
-            .collect(Collectors.toList());
 
     /**
      * 割引を適用可能か？
